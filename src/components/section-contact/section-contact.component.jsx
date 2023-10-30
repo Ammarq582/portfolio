ea59d84c212b9e28b8d9b856acf97aa31151ab03
Ammarq582 <ammarq582@gmail.com>
2023-10-31 04:23:17 +0500

    .

--- src/components/section-contact/section-contact.component.jsx
+++ src/components/section-contact/section-contact.component.jsx
@@ -11,8 +11,8 @@ const SectionContact = () => {
                     <h2>Contact</h2>
                     <p>Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
                 </HeadingPrimary>
-                <form name='contact' method='post' className="contact-form">
-                    <input type='hidden' name='form-name' value='contact'/>
+                <form name="contact" method="post" className="contact-form">
+                    <input type="hidden" name="form-name" value='contact'/>
                     <FormInput
                     label='Name'
                     type='text'
