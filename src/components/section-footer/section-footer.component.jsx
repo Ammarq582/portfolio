780c7d25cd134297b9260601aafe58f22cd47643
Ammarq582 <ammarq582@gmail.com>
2024-01-14 05:11:27 +0500

    added a new project

--- src/components/section-footer/section-footer.component.jsx
+++ src/components/section-footer/section-footer.component.jsx
@@ -16,10 +16,10 @@ const SectionFooter = () => {
                     <div className="social-links">
                         <h2>Social</h2>
                         <div className="social-links-container">
-                            <Link className='link'>
+                            <Link className='link' to='https://www.linkedin.com/in/ammar-qureshi-79a0b91b6' target='_blank'>
                                 <img src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png" alt="linkedin" />
                             </Link>
-                            <Link className='link' to="https://github.com/Ammarq582?tab=repositories">
+                            <Link className='link' to="https://github.com/Ammarq582?tab=repositories" target='_blank'>
                                 <img src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png" alt="linkedin" />
                             </Link>
                         </div>
--- src/components/section-projects/section-projects.component.jsx
+++ src/components/section-projects/section-projects.component.jsx
@@ -5,30 +5,24 @@ import './section-projects.styles.scss'
 
 
 const projectsList = [
-    {
-      name: 'Your Clothing',
-      description: 'Your Clothing is a demo webapp for online clothing store template and I created this webapp from scratch using the frontend tools I know.',
-      imageUrl: 'https://i.ibb.co/7J7Hpds/Your-Clothing.jpg',
-      linkAddress: 'https://yourclothing.netlify.app/',
-    },
-    {
-      name: 'Booking Tours',
-      description: 'Booking Tours is a demo website for booking tours and I created this beautiful website from scratch using the frontend tools I know.',
-      imageUrl: 'https://i.ibb.co/y6JcH9W/Booking-Tours.jpg',
-      linkAddress: 'https://booking-tours.netlify.app/',
-    },
-    {
-      name: 'Nexters',
-      description: "Nexters is a demo website for real estate business and I created this website from scratch using the frontend tools I know.",
-      imageUrl: 'https://i.ibb.co/8NTQmbJ/Nexters.jpg',
-      linkAddress: 'https://mynexters.netlify.app/',
-    },
-    {
-      name: 'Trillo',
-      description: 'Trillo is a demo website for bookings hotels and I created this website from scratch using the frontend tools I know.',
-      imageUrl: 'https://i.ibb.co/WPh1ySK/Trillo.jpg',
-      linkAddress: 'https://mytrillos.netlify.app/',
-    },
+  {
+    name: 'MyFlix',
+    description: "MyFlix is a free streaming web app that i developed using TMDB API that allows you to watch your favorite shows and movies for free",
+    imageUrl: 'https://i.ibb.co/ccsJwFN/Myflix.jpg',
+    linkAddress: 'https://mynexters.netlify.app/',
+  },
+  {
+    name: 'Your Clothing',
+    description: 'Your Clothing is a demo webapp for online clothing store template and I created this webapp from scratch using the frontend tools I know.',
+    imageUrl: 'https://i.ibb.co/7J7Hpds/Your-Clothing.jpg',
+    linkAddress: 'https://yourclothing.netlify.app/',
+  },
+  {
+    name: 'Booking Tours',
+    description: 'Booking Tours is a demo website for booking tours and I created this beautiful website from scratch using the frontend tools I know.',
+    imageUrl: 'https://i.ibb.co/y6JcH9W/Booking-Tours.jpg',
+    linkAddress: 'https://booking-tours.netlify.app/',
+  },
 ]
 
 const SectionProjects = () => {
